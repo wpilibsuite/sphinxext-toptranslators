a926aa5055b1b0ad555711b57f521fd148d97b5b
Daltz333 <darasmith@wccnet.edu>
2020-07-09 20:08:21 -0400

    Uncomment auto-versioning

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ import setuptools
 
 # This will fail if something happens or if not in a git repository.
 # This is intentional.
-#ret = subprocess.run("git describe --tags --abbrev=0", stdout=subprocess.PIPE,
-#                     stderr=subprocess.PIPE, check=True, shell=True)
-#version = ret.stdout.decode("utf-8").strip()
-version = "0.0.1"
+ret = subprocess.run("git describe --tags --abbrev=0", stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE, check=True, shell=True)
+version = ret.stdout.decode("utf-8").strip()
 
 with open("README.md", 'r', encoding="utf-8") as readme:
     long_description = readme.read()
